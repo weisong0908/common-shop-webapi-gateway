9e37f6386f26c1704d08b03e689d93f55a942fdc
Teng Wei Song <weisong.teng.work@gmail.com>
2020-12-05 11:44:58 +0800

    add staging environment

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace CommonShop.WebApiGateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
