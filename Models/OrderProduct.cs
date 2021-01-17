7aea4258e9e691f589eaff4c5fce7be10dbc905c
Teng Wei Song <weisong.teng.work@gmail.com>
2021-01-17 17:41:59 +0800

    include product detail in order query

--- Models/OrderProduct.cs
+++ Models/OrderProduct.cs
@@ -6,7 +6,6 @@ namespace CommonShop.WebApiGateway.Models
     public class OrderProduct
     {
         public Product Product { get; set; }
-        public Guid ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal Amount { get; set; }
     }
