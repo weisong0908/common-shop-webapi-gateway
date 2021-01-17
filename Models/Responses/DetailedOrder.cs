9559b1c9601fb939878b63e77f0c05b19c57dac7
Teng Wei Song <weisong.teng.work@gmail.com>
2021-01-17 20:04:14 +0800

    remove shipping address from detailed order

--- Models/Responses/DetailedOrder.cs
+++ Models/Responses/DetailedOrder.cs
@@ -9,7 +9,6 @@ namespace CommonShop.WebApiGateway.Models.Responses
         public DateTime Date { get; set; }
         public IList<OrderItem> Products { get; set; }
         public Customer Customer { get; set; }
-        public Address ShippingAddress { get; set; }
         public IList<Fee> Fees { get; set; }
         public decimal TotalPrice { get; set; }
         public string OrderStatus { get; set; }
